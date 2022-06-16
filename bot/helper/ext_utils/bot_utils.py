864f120d0e7d7534fa5df6cf960bc9a3fd39e7b8
ytubeupX <uploadmyvie41@gmail.com>
2022-06-16 23:34:20 +0600

    your message

---
+++ bot/helper/ext_utils/bot_utils.py
@@ -0,0 +1,320 @@
+import logging
+import re
+import threading
+import time
+import math
+
+from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot import dispatcher, download_dict, download_dict_lock, STATUS_LIMIT
+from telegram import InlineKeyboardMarkup
+from telegram.ext import CallbackQueryHandler
+from bot.helper.telegram_helper import button_build, message_utils
+
+
+
+
+LOGGER = logging.getLogger(__name__)
+
+MAGNET_REGEX = r"magnet:\?xt=urn:btih:[a-zA-Z0-9]*"
+
+URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
+
+COUNT = 0
+PAGE_NO = 1
+
+
+
+class MirrorStatus:
+    STATUS_UPLOADING = "Uploading...📤"
+    STATUS_DOWNLOADING = "Downloading...📥"
+    STATUS_CLONING = "Cloning...♻️"
+    STATUS_WAITING = "Queued...📝"
+    STATUS_FAILED = "Failed 🚫. Cleaning Download..."
+    STATUS_PAUSE = "Paused...⭕️"
+    STATUS_ARCHIVING = "Archiving...🔐"
+    STATUS_EXTRACTING = "Extracting...📂"
+    STATUS_SPLITTING = "Splitting...✂️"
+
+PROGRESS_MAX_SIZE = 100 // 8
+PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
+
+
+
+
+
+
+class setInterval:
+    def __init__(self, interval, action):
+        self.interval = interval
+        self.action = action
+        self.stopEvent = threading.Event()
+        thread = threading.Thread(target=self.__setInterval)
+        thread.start()
+
+    def __setInterval(self):
+        nextTime = time.time() + self.interval
+        while not self.stopEvent.wait(nextTime - time.time()):
+            nextTime += self.interval
+            self.action()
+
+    def cancel(self):
+        self.stopEvent.set()
+        
+        
+        
+        
+
+
+
+def getDownloadByGid(gid):
+    with download_dict_lock:
+        for dl in download_dict.values():
+            status = dl.status()
+            if (
+                status
+                not in [
+                    MirrorStatus.STATUS_ARCHIVING,
+                    MirrorStatus.STATUS_EXTRACTING,
+                    MirrorStatus.STATUS_SPLITTING
+                ]
+                and dl.gid() == gid
+            ):
+                return dl
+    return None
+
+
+
+
+
+
+def get_readable_file_size(size_in_bytes) -> str:
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024:
+        size_in_bytes /= 1024
+        index += 1
+    try:
+        return f'{round(size_in_bytes, 2)}{SIZE_UNITS[index]}'
+    except IndexError:
+        return 'File too large'
+        
+        
+
+
+
+def getAllDownload():
+    with download_dict_lock:
+        for dlDetails in download_dict.values():
+            status = dlDetails.status()
+            if (
+                status
+                not in [
+                    MirrorStatus.STATUS_ARCHIVING,
+                    MirrorStatus.STATUS_EXTRACTING,
+                    MirrorStatus.STATUS_SPLITTING,
+                    MirrorStatus.STATUS_CLONING,
+                    MirrorStatus.STATUS_UPLOADING,
+                ]
+                and dlDetails
+            ):
+                return dlDetails
+    return None
+  
+  
+  
+  
+  
+  
+ 
+def get_progress_bar_string(status):
+    completed = status.processed_bytes() / 8
+    total = status.size_raw() / 8
+    p = 0 if total == 0 else round(completed * 100 / total)
+    p = min(max(p, 0), 100)
+    cFull = p // 8
+    cPart = p % 8 - 1
+    p_str = '█' * cFull
+    if cPart >= 0:
+        p_str += PROGRESS_INCOMPLETE[cPart]
+    p_str += ' ' * (PROGRESS_MAX_SIZE - cFull)
+    p_str = f"[{p_str}]"
+    return p_str
+    
+    
+
+
+
+def get_readable_message():
+    with download_dict_lock:
+        msg = ""
+        INDEX = 0
+        if STATUS_LIMIT is not None:
+            dick_no = len(download_dict)
+            global pages
+            pages = math.ceil(dick_no/STATUS_LIMIT)
+            if PAGE_NO > pages and pages != 0:
+                globals()['COUNT'] -= STATUS_LIMIT
+                globals()['PAGE_NO'] -= 1
+        for download in list(download_dict.values()):
+            INDEX += 1
+            if INDEX > COUNT:
+                msg += f"<b>📁 Filename :</b> <code>{download.name()}</code>"
+                msg += f"\n<b>ℹ️ Status :</b> <i>{download.status()}</i>"
+                if download.status() not in [
+                    MirrorStatus.STATUS_ARCHIVING,
+                    MirrorStatus.STATUS_EXTRACTING,
+                ]:
+                    msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>"
+                    if download.status() == MirrorStatus.STATUS_DOWNLOADING:
+                        msg += f"\n<b>📥 Downloaded :</b> {get_readable_file_size(download.processed_bytes())}<b>\n💾 Size</b>: {download.size()}"
+                    elif download.status() == MirrorStatus.STATUS_CLONING:
+                        msg += f"\n<b>♻️ Uploaded :</b> {get_readable_file_size(download.processed_bytes())}<b>\n<b>⚙️ Engine: Rclone</b>\n💾 Size</b>: {download.size()}"
+                    else:
+                        msg += f"\n<b>📤 Downloaded :</b> {get_readable_file_size(download.processed_bytes())}<b>\n<b>⚙️ Engine: Rclone</b>\n💾 Size</b>: {download.size()}"
+                    msg += f"\n<b>⚡ Speed :</b> {download.speed()}" \
+                            f"\n<b>⏲️ ETA :</b> {download.eta()} "
+                    # if hasattr(download, 'is_torrent'):
+                    try:
+                        msg += f"\n<b>👥 User :</b> <b>{download.message.from_user.first_name}</b>\n<b>⚠️ Warn:</b><code>/warn {download.message.from_user.id}</code>"
+                    except:
+                        pass
+                    try:
+                        msg += f"\n<b>⚙️ Engine : Aria2</b>\n<b>📶 Ping :</b> {download.aria_download().connections}"
+                    except:
+                        pass
+                    try:
+                        msg += f"\n<b>🌱 Seeder :</b> <code>{download.aria_download().num_seeders}</code>" \
+                                f"\n<b>✳️ Peers :</b> <code>{download.aria_download().connections}</code>"
+                    except:
+                        pass
+                    try:
+                        msg += f"\n<b>🌱 Seeders :</b> <code>{download.torrent_info().num_seeds}</code>" \
+                            f" | <b>🧲 Leechers :</b> <code>{download.torrent_info().num_leechs}</code>"
+                    except:
+                        pass    
+                    try:
+                        msg += f"\n<b>⚙️ Engine : Qbit</b>\n<b>🌍:</b> {download.torrent_info().num_leechs}"
+                    except:
+                        pass
+                    msg += f"\n<b>⛔ To Stop :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += "\n\n"
+                if STATUS_LIMIT is not None and INDEX >= COUNT + STATUS_LIMIT:
+                    break
+        if STATUS_LIMIT is not None:
+            if INDEX > COUNT + STATUS_LIMIT:
+                return None, None
+            if dick_no > STATUS_LIMIT:
+                msg += f"<b>📑 Page :</b> <code>{PAGE_NO}/{pages}</code> | <b>📝 ᴛᴀꜱᴋꜱ :</b> <code>{dick_no}</code>\n"
+                buttons = button_build.ButtonMaker()
+                buttons.sbutton("⬅️", "pre")
+                buttons.sbutton("➡️", "nex")
+                button = InlineKeyboardMarkup(buttons.build_menu(2))
+                return msg, button
+        return msg, ""
+
+
+
+
+
+def get_readable_time(seconds: int) -> str:
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+    
+ 
+ 
+ 
+ 
+ 
+
+def turn(update, context):
+    query = update.callback_query
+    query.answer()
+    global COUNT, PAGE_NO
+    if query.data == "nex":
+        if PAGE_NO == pages:
+            COUNT = 0
+            PAGE_NO = 1
+        else:
+            COUNT += STATUS_LIMIT
+            PAGE_NO += 1
+    elif query.data == "pre":
+        if PAGE_NO == 1:
+            COUNT = STATUS_LIMIT * (pages - 1)
+            PAGE_NO = pages
+        else:
+            COUNT -= STATUS_LIMIT
+            PAGE_NO -= 1
+    message_utils.update_all_messages()
+
+
+
+def is_url(url: str):
+    url = re.findall(URL_REGEX, url)
+    return bool(url)
+
+def is_gdrive_link(url: str):
+    return "drive.google.com" in url
+
+def is_gdtot_link(url: str):
+    url = re.match(r'https?://.*\.gdtot\.\S+', url)
+    return bool(url)
+
+
+def is_mega_link(url: str):
+    return "mega.nz" in url or "mega.co.nz" in url
+
+
+def get_mega_link_type(url: str):
+    if "folder" in url:
+        return "folder"
+    elif "file" in url:
+        return "file"
+    elif "/#F!" in url:
+        return "folder"
+    return "file"
+
+
+def is_magnet(url: str):
+    magnet = re.findall(MAGNET_REGEX, url)
+    return bool(magnet)
+
+
+
+
+
+def new_thread(fn):
+    """To use as decorator to make a function call threaded.
+    Needs import
+    from threading import Thread"""
+
+    def wrapper(*args, **kwargs):
+        thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        thread.start()
+        return thread
+
+    return wrapper
+    
+ 
+ 
+ 
+ 
+next_handler = CallbackQueryHandler(turn, pattern="nex", run_async=True)
+previous_handler = CallbackQueryHandler(turn, pattern="pre", run_async=True)
+dispatcher.add_handler(next_handler)
+dispatcher.add_handler(previous_handler)
---
+++ bot/helper/ext_utils/fs_utils.py
@@ -0,0 +1,242 @@
+import sys
+import shutil
+import os
+import pathlib
+import magic
+import tarfile
+import subprocess
+import time
+import math
+import json
+
+from PIL import Image
+
+from .exceptions import NotSupportedExtractionArchive
+from bot import aria2, LOGGER, DOWNLOAD_DIR, get_client, TG_SPLIT_SIZE, EQUAL_SPLITS
+
+VIDEO_SUFFIXES = ("M4V", "MP4", "MOV", "FLV", "WMV", "3GP", "MPG", "WEBM", "MKV", "AVI")
+
+def clean_download(path: str):
+    if os.path.exists(path):
+        LOGGER.info(f"Cleaning Download: {path}")
+        shutil.rmtree(path)
+
+def start_cleanup():
+    try:
+        shutil.rmtree(DOWNLOAD_DIR)
+    except FileNotFoundError:
+        pass
+
+def clean_all():
+    aria2.remove_all(True)
+    get_client().torrents_delete(torrent_hashes="all", delete_files=True)
+    get_client().auth_log_out()
+    try:
+        shutil.rmtree(DOWNLOAD_DIR)
+    except FileNotFoundError:
+        pass
+
+def exit_clean_up(signal, frame):
+    try:
+        LOGGER.info("Please wait, while we clean up the downloads and stop running downloads")
+        clean_all()
+        sys.exit(0)
+    except KeyboardInterrupt:
+        LOGGER.warning("Force Exiting before the cleanup finishes!")
+        sys.exit(1)
+
+def get_path_size(path):
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    total_size = 0
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            abs_path = os.path.join(root, f)
+            total_size += os.path.getsize(abs_path)
+    return total_size
+    
+def tar(org_path):
+    tar_path = org_path + ".tar"
+    path = pathlib.PurePath(org_path)
+    LOGGER.info(f'Tar: orig_path: {org_path}, tar_path: {tar_path}')
+    tar = tarfile.open(tar_path, "w")
+    tar.add(org_path, arcname=path.name)
+    tar.close()
+    return tar_path
+    
+
+def get_base_name(orig_path: str):
+    if orig_path.endswith(".tar.bz2"):
+        return orig_path.replace(".tar.bz2", "")
+    elif orig_path.endswith(".tar.gz"):
+        return orig_path.replace(".tar.gz", "")
+    elif orig_path.endswith(".bz2"):
+        return orig_path.replace(".bz2", "")
+    elif orig_path.endswith(".gz"):
+        return orig_path.replace(".gz", "")
+    elif orig_path.endswith(".tar.xz"):
+        return orig_path.replace(".tar.xz", "")
+    elif orig_path.endswith(".tar"):
+        return orig_path.replace(".tar", "")
+    elif orig_path.endswith(".tbz2"):
+        return orig_path.replace("tbz2", "")
+    elif orig_path.endswith(".tgz"):
+        return orig_path.replace(".tgz", "")
+    elif orig_path.endswith(".zip"):
+        return orig_path.replace(".zip", "")
+    elif orig_path.endswith(".7z"):
+        return orig_path.replace(".7z", "")
+    elif orig_path.endswith(".Z"):
+        return orig_path.replace(".Z", "")
+    elif orig_path.endswith(".rar"):
+        return orig_path.replace(".rar", "")
+    elif orig_path.endswith(".iso"):
+        return orig_path.replace(".iso", "")
+    elif orig_path.endswith(".wim"):
+        return orig_path.replace(".wim", "")
+    elif orig_path.endswith(".cab"):
+        return orig_path.replace(".cab", "")
+    elif orig_path.endswith(".apm"):
+        return orig_path.replace(".apm", "")
+    elif orig_path.endswith(".arj"):
+        return orig_path.replace(".arj", "")
+    elif orig_path.endswith(".chm"):
+        return orig_path.replace(".chm", "")
+    elif orig_path.endswith(".cpio"):
+        return orig_path.replace(".cpio", "")
+    elif orig_path.endswith(".cramfs"):
+        return orig_path.replace(".cramfs", "")
+    elif orig_path.endswith(".deb"):
+        return orig_path.replace(".deb", "")
+    elif orig_path.endswith(".dmg"):
+        return orig_path.replace(".dmg", "")
+    elif orig_path.endswith(".fat"):
+        return orig_path.replace(".fat", "")
+    elif orig_path.endswith(".hfs"):
+        return orig_path.replace(".hfs", "")
+    elif orig_path.endswith(".lzh"):
+        return orig_path.replace(".lzh", "")
+    elif orig_path.endswith(".lzma"):
+        return orig_path.replace(".lzma", "")
+    elif orig_path.endswith(".lzma2"):
+        return orig_path.replace(".lzma2", "")
+    elif orig_path.endswith(".mbr"):
+        return orig_path.replace(".mbr", "")
+    elif orig_path.endswith(".msi"):
+        return orig_path.replace(".msi", "")
+    elif orig_path.endswith(".mslz"):
+        return orig_path.replace(".mslz", "")
+    elif orig_path.endswith(".nsis"):
+        return orig_path.replace(".nsis", "")
+    elif orig_path.endswith(".ntfs"):
+        return orig_path.replace(".ntfs", "")
+    elif orig_path.endswith(".rpm"):
+        return orig_path.replace(".rpm", "")
+    elif orig_path.endswith(".squashfs"):
+        return orig_path.replace(".squashfs", "")
+    elif orig_path.endswith(".udf"):
+        return orig_path.replace(".udf", "")
+    elif orig_path.endswith(".vhd"):
+        return orig_path.replace(".vhd", "")
+    elif orig_path.endswith(".xar"):
+        return orig_path.replace(".xar", "")
+    else:
+        raise NotSupportedExtractionArchive('File format not supported for extraction')
+
+def get_mime_type(file_path):
+    mime = magic.Magic(mime=True)
+    mime_type = mime.from_file(file_path)
+    mime_type = mime_type or "text/plain"
+    return mime_type
+
+
+
+
+def take_ss(video_file):
+    des_dir = 'Thumbnails'
+    if not os.path.exists(des_dir):
+        os.mkdir(des_dir)
+    des_dir = os.path.join(des_dir, f"{time.time()}.jpg")
+    duration = get_media_info(video_file)[0]
+    if duration == 0:
+        duration = 3
+    duration = duration // 2
+    subprocess.run(["ffmpeg", "-hide_banner", "-loglevel", "error", "-ss", str(duration),
+                    "-i", video_file, "-vframes", "1", des_dir])
+    if not os.path.lexists(des_dir):
+        return None
+
+    Image.open(des_dir).convert("RGB").save(des_dir)
+    img = Image.open(des_dir)
+    img.resize((480, 320))
+    img.save(des_dir, "JPEG")
+    return des_dir
+    
+
+
+
+
+
+def split(path, size, filee, dirpath, split_size, start_time=0, i=1):
+    if filee.upper().endswith(VIDEO_SUFFIXES):
+        base_name, extension = os.path.splitext(filee)
+        parts = math.ceil(size/TG_SPLIT_SIZE)
+        if EQUAL_SPLITS:
+            split_size = (size // parts) - 2500000
+        else:
+            split_size = split_size - 2500000
+        while i <= parts :
+            parted_name = "{}.part{}{}".format(str(base_name), str(i).zfill(3), str(extension))
+            out_path = os.path.join(dirpath, parted_name)
+            subprocess.run(["ffmpeg", "-hide_banner", "-loglevel", "error", "-i", 
+                            path, "-ss", str(start_time), "-fs", str(split_size),
+                            "-async", "1", "-strict", "-2", "-c", "copy", out_path])
+            out_size = get_path_size(out_path)
+            if out_size > 2097152000:
+                dif = out_size - TG_SPLIT_SIZE
+                split_size = split_size - dif + 2400000
+                os.remove(out_path)
+                return split(path, size, filee, dirpath, split_size, start_time, i)
+            lpd = get_media_info(out_path)[0]
+            start_time += lpd - 3
+            i = i + 1
+    else:
+        out_path = os.path.join(dirpath, filee + ".")
+        subprocess.run(["split", "--numeric-suffixes=1", "--suffix-length=3", f"--bytes={split_size}", path, out_path])
+
+
+
+
+
+def get_media_info(path):
+    try:
+        result = subprocess.check_output(["ffprobe", "-hide_banner", "-loglevel", "error", "-print_format", 
+                                          "json", "-show_format", path]).decode('utf-8')
+        fields = json.loads(result)['format']
+    except Exception as e:
+        LOGGER.error(str(e))
+        return 0, None, None
+    try:
+        duration = round(float(fields['duration']))
+    except:
+        duration = 0
+    try:
+        artist = str(fields['tags']['artist'])
+    except:
+        artist = None
+    try:
+        title = str(fields['tags']['title'])
+    except:
+        title = None
+    return duration, artist, title
+
+
+
+
+def safe_filename(path_):
+    if path_ is None:
+        return
+    safename = path_.replace("'", "").replace('"', "")
+    if safename != path_:
+        os.rename(path_, safename)
+    return safename
